608dabe309497e9272cbc717447fecad1366f8f1
rblhlu <79634724+rblhlu@users.noreply.github.com>
2022-01-13 08:39:28 +0300

    secondary commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React from "react";
-
-// We use Route in order to define the different routes of our application
-import { Route, Routes } from "react-router-dom";
-
-// We import all the components we need in our app
-import Navbar from "./components/navbar";
-import RecordList from "./components/recordList";
-import Edit from "./components/edit";
-import Create from "./components/create";
-
-
 function App() {
   return (
     <div className="App">
@@ -30,16 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-
-      <div>
-          <Navbar />
-          <Routes>
-            <Route exact path="/" element={<RecordList />} />
-            <Route path="/edit/:id" element={<Edit />} />
-            <Route path="/create" element={<Create />} />
-          </Routes>
-      </div>
-
     </div>
   );
 }
